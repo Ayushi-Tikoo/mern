86fcadd8722b03e1ac56c374ebe7c385477ff630
Ayushi-Tikoo <ayushi.tikoo@thegatewaycorp.com>
2021-03-11 13:05:39 +0530

    commmit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get('/', (req, res) => res.send('API Running successfullly'));
 
 //it will look for an env variable called port if no port is set it will go to 5000
 const PORT = process.env.PORT || 5000;
