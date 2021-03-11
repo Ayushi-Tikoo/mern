bf6c512d824b199cd755360cfa2a8c7898434f33
Ayushi-Tikoo <ayushi.tikoo@thegatewaycorp.com>
2021-03-11 18:04:00 +0530

    Commit2

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 
+//Connect to database
+connectDB();
+
+//Init Middleware
+//it's like body parser so that we can parse through the body
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 //it will look for an env variable called port if no port is set it will go to 5000
 const PORT = process.env.PORT || 5000;
